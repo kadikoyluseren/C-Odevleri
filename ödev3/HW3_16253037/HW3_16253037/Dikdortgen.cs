18788b4f2c7b4b462ab8b5cf456aa12307aa511b
kadikoyluseren <kadikoyluseren@gmail.com>
2018-05-05 01:51:46 +0300

    Nesneye Yönelik Programlama 3.Ödev

---
+++ ödev3/HW3_16253037/HW3_16253037/Dikdortgen.cs
@@ -0,0 +1,83 @@
+﻿
+// AD SOYAD : SEREN KADIKÖYLÜ
+// ÖĞRENCİ NUMARASI : 16253037
+
+using System;
+namespace HW3_16253037
+{
+    class Dikdortgen
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Dikdortgen()
+        {
+            this.X = 0;
+            this.Y = 0;
+        }
+
+        public Dikdortgen(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        private int en;
+        private int boy;
+        public int En
+        {
+            get
+            {
+                return en;
+            }
+            set
+            {
+                if (value > 0)
+                    en = value;
+
+            }
+        }
+        public int Boy
+        {
+            get
+            {
+                return boy;
+            }
+            set
+            {
+                if (value > 0)
+                    boy = value;
+            }
+        }
+
+        public int Alan()
+        {
+            return en * boy;
+        }
+
+        public int Cevre()
+        {
+            return 2 * (en + boy);
+        }
+
+        public void Hareket(int xx, int yy)
+        {
+            X = X + xx;
+            Y = Y + yy;
+            Console.WriteLine("\n"+xx+ " BIRIM X, " + yy+ " BIRIM Y DOGRULTUSUNDA HAREKET SONUCU KOSE KOORDINATLARI");
+        }
+        
+        public void KoordinatYazdir()
+        {
+            Console.WriteLine(String.Format("{0,-9}{1,9}", "(" + X + "," + (Y + boy) + ")", "(" + (X + en) + "," + (Y + boy) + ")"));
+            Console.WriteLine(String.Format("{0,-9}{1,9}", "(" + X + "," + Y + ")", "(" + (X + en) + "," + Y + ")"));
+        }
+
+        public void CevreAlanYazdir()
+        {
+            Console.WriteLine("\nCevre = " + Cevre());
+            Console.WriteLine("Alan = " + Alan());
+        }
+
+    }
+}
---
+++ ödev3/HW3_16253037/HW3_16253037/DikdortgenTest.cs
@@ -0,0 +1,40 @@
+﻿
+// AD SOYAD : SEREN KADIKÖYLÜ
+// ÖĞRENCİ NUMARASI : 16253037
+
+using System;
+namespace HW3_16253037
+{
+    class DikdortgenTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIKDORTGENIN HAREKET ETTIRILMESI");
+            Console.Write("Kac ornek gormek istersiniz : ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Dikdortgen dik = new Dikdortgen();
+            Dikdortgen[] dizi = new Dikdortgen[x];
+            Random rast = new Random();
+            
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine("________________________________________________________________________________");
+                dik.En = rast.Next(1, 100);
+                dik.Boy = rast.Next(1, 100);
+                dik.X = rast.Next(-100,100);
+                dik.Y = rast.Next(-100, 100);
+                Console.WriteLine("\nKOSELERIN ILK KOORDINATLARI");
+                dik.KoordinatYazdir();
+                dik.Hareket(rast.Next(-100,100),rast.Next(-100,100));
+                dik.KoordinatYazdir();
+                Console.WriteLine("\nEn = "+dik.En);
+                Console.Write("Boy= "+dik.Boy);
+                dik.CevreAlanYazdir();
+
+            }Console.WriteLine("________________________________________________________________________________");
+
+            Console.ReadLine();
+        }
+    }
+}
